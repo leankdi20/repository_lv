cd97a0b87e9657bc9703642eeb10bbccc8966bd0
Varela Leandro <98416431+leankdi20@users.noreply.github.com>
2025-05-01 13:06:52 -0600

    nuevos arreglos de rutas

--- app/routes.py
+++ app/routes.py
@@ -3,28 +3,28 @@ from datetime import datetime
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route('/', strict_slashes=False)
 def home():
     return render_template('home.html')  
 
-@main.route('/projects')
+@main.route('/projects', strict_slashes=False)
 def projects():
     return render_template('projects.html')
 
-@main.route('/about')
+@main.route('/about', strict_slashes=False)
 def about():
     return render_template('about.html')
 
 
-@main.route('/skills')
+@main.route('/skills', strict_slashes=False)
 def skills():
     return render_template('skills.html')
 
-@main.route('/experience')
+@main.route('/experience', strict_slashes=False)
 def experience():
     return render_template('experience.html')
 
-@main.route('/contact')
+@main.route('/contact', strict_slashes=False)
 def contact():
     return render_template('contact.html')
 
