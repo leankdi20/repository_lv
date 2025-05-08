dd85d597ae772777506647a09084a24fcd1ee065
Varela Leandro <98416431+leankdi20@users.noreply.github.com>
2025-05-08 14:45:05 -0600

    ultima implementacion email.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,27 @@
 from flask import Flask
 from datetime import datetime
 from app.routes import main  
+from flask_mail import Mail, Message
+from .extensions import mail  
 # from .routes import main
 '''El import from .routes import main solo funciona si app es tratado como un paquete Python (por ejemplo, cuando Flask ejecuta create_app() desde run.py).'''
 
 def create_app():
     app = Flask(__name__)
+
+    app.secret_key = '7b6cba071352bf3d85de263b736b5f171e877dae66082bf19489b4715bae9f01'
+    # Configuración del correo (ejemplo con Gmail)
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'leanvarela6@gmail.com'  # Tu correo
+    app.config['MAIL_PASSWORD'] = 'lruabarhfquhqxsf'  # Tu contraseña de aplicación
+    app.config['MAIL_DEFAULT_SENDER'] = 'leanvarela6@gmail.com'
+
     app.register_blueprint(main)
+
+    
+    mail.init_app(app)
 
     @app.context_processor
     def inject_year():
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, flash, url_for
 from datetime import datetime
+from app.extensions import mail
+from flask_mail import Message
 
 main = Blueprint('main', __name__)
 
@@ -24,7 +26,33 @@ def skills():
 def experience():
     return render_template('experience.html')
 
-@main.route('/contact', strict_slashes=False)
+@main.route('/message', strict_slashes=False)
+def message():
+    return render_template('message_email.html')
+
+
+@main.route('/contact', methods=['GET', 'POST'], strict_slashes=False)
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        mensaje = request.form['mensaje']
+
+        msg = Message(
+            subject=f"Nuevo mensaje de {nombre}",
+            sender=correo,
+            recipients=['leanvarela6@gmail.com'],
+            body=f"Nombre: {nombre}\nCorreo: {correo}\n\nMensaje:\n{mensaje}"
+        )
+
+        try:
+            mail.send(msg)
+            flash("¡Mensaje enviado exitosamente!")
+        except Exception as e:
+            flash(f"Error al enviar mensaje: {str(e)}")
+
+        return redirect(url_for('main.message'))
+
+    return render_template("contact.html")
+
 
